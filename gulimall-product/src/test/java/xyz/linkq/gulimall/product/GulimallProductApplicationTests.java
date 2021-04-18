4f142a7f9330a17f9325644b7ea806d2ed8f3387
linkq <mrlinkq@hotmail.com>
2021-04-18 22:18:06 +0800

    product模块生产完毕

--- gulimall-product/src/test/java/xyz/linkq/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/xyz/linkq/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,34 @@
 package xyz.linkq.gulimall.product;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import xyz.linkq.gulimall.product.entity.BrandEntity;
+import xyz.linkq.gulimall.product.service.BrandService;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setName("苹果");
+//        brandEntity.setDescript("我是一个小啊小苹果");
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功！");
+//
+//        brandService.updateById(brandEntity);
+//        List<BrandEntity> list = brandService.list();
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i).getName());
+//        }
     }
 
 }
